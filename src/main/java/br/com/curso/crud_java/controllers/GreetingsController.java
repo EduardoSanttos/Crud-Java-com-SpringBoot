db9bf6cb53f4393bac4117cae68923f1fd4dc2d7
Eduardo Santos <f.eduardo.santoos@gmail.com>
2023-02-20 23:24:06 -0300

    Criado end point para listar todos os usuarios

--- src/main/java/br/com/curso/crud_java/controllers/GreetingsController.java
+++ src/main/java/br/com/curso/crud_java/controllers/GreetingsController.java
@@ -1,10 +1,15 @@
 package br.com.curso.crud_java.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,5 +36,14 @@ public class GreetingsController {
     public String greetingText(@PathVariable String name) {
     	
         return "Hello " + name + "!";
+    }
+    
+    @GetMapping(value = "listarTodos")
+    @ResponseBody // Retorna os dados para o corpo da resposta
+    public ResponseEntity<List<Usuario>> listUsuario(){
+    	
+    	List<Usuario> usuarios = usuarioRepository.findAll();
+    	
+    	return new ResponseEntity<List<Usuario>>(usuarios, HttpStatus.OK);//retorna um json
     }
 }
