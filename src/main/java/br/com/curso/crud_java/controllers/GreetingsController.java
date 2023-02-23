a5b2083582dd8143151a82fef0123c087dc2951e
Eduardo Santos <f.eduardo.santoos@gmail.com>
2023-02-23 17:05:54 -0300

    Criado end-point para buscar e atualizar no banco

--- src/main/java/br/com/curso/crud_java/controllers/GreetingsController.java
+++ src/main/java/br/com/curso/crud_java/controllers/GreetingsController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -68,4 +69,33 @@ public class GreetingsController {
 
 		return new ResponseEntity<String>("Usuario deletado com sucesso!", HttpStatus.OK);
 	}
+	
+	@PutMapping(value = "atualizar") // Mapeia a URL
+	@ResponseBody // Descrição da resposta
+	public ResponseEntity<?> atualizar(@RequestBody Usuario usuario) { // Recebi os dados para salvar
+
+		if(usuario.getId() == null) {
+			
+			return new ResponseEntity<String>("Id não informado!", HttpStatus.OK);
+		}
+		Usuario user = usuarioRepository.saveAndFlush(usuario);
+		return new ResponseEntity<Usuario>(user, HttpStatus.OK);
+
+	}
+	
+	@GetMapping(value = "buscarUsuarioId") 
+	@ResponseBody
+	public ResponseEntity<Usuario> BuscarUsuarioId(@RequestParam(name = "idUser") Long idUser) {
+
+		Usuario user = usuarioRepository.findById(idUser).get();
+
+		return new ResponseEntity<Usuario>(user, HttpStatus.OK);
+	}
 }
+
+
+
+
+
+
+
